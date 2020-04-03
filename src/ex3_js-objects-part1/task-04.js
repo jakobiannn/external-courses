1e1f962e9bca736f5ccce3fc1619395aac4804fd
jakobiannn <mozilla322@gmail.com>
2020-04-04 00:12:05 +0300

    fixed

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,11 +1,12 @@
 function checkOrAddProperty(prop, obj){
   for(key in obj){
 	  if(obj.hasOwnProperty(key)){
-     	if (prop !== key){
-        obj[prop] = 'new'; //eslint-disable-line no-param-reassign
+     	if (prop === key){
+          return obj;
       }
     }	
   }
+  obj[prop] = 'new'; //eslint-disable-line no-param-reassign
   return obj;
 }
 module.exports = checkOrAdd;
