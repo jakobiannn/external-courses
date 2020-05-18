303a1a4c4016d0da933b09a2f3e789c1621fbcd6
jakobiannn <mozilla322@gmail.com>
2020-05-18 19:00:29 +0300

    fix

--- src/ex14_js-context/task-01.js
+++ src/ex14_js-context/task-01.js
@@ -1,41 +1,41 @@
 let Calculator = {
-  result: undefined,
+  result: 0,
 
   multiply(num = 1){
-    if (this.result === undefined) { this.result = 1;}
+    if (this.result === 0) { this.result = 0;}
     this.result *= num;
-    return Calculator;
+    return this;
   },
   getResult() {
     return this.result;
   },
   add(num = 0) {
-    if (this.result === undefined) { this.result = 0;}
+    // if (this.result === 0) { this.result = 0;}
     this.result += num;
-    return Calculator;
+    return this;
   },
   subtract(num = 0) {
-    if (this.result === undefined) { this.result = 0;}
+    // if (this.result === 0) { this.result = 0;}
     this.result -= num;
-    return Calculator;
+    return this;
   },
   divide(num = 1) {
-    if (this.result === undefined) { this.result = 0;}
+    // if (this.result === 0) { this.result = 0;}
     this.result /= num;
-    return Calculator;
+    return this;
   },
   reset() {
     this.result = 0;
-    return Calculator;
+    return this;
   },
   setState(num = 0) {
     this.result = num;
-    return Calculator;
+    return this;
   },
   fetchData(cb) {
     this.result = 500;
     setTimeout(cb, 3000, 500);
-    return Calculator;
+    return this;
   }
 }
 
