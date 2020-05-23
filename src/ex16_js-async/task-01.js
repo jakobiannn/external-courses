5738d5593f6b6d4162acb938a74b43d0174b3115
jakobiannn <mozilla322@gmail.com>
2020-05-24 00:05:35 +0300

    rework

--- src/ex16_js-async/task-01.js
+++ src/ex16_js-async/task-01.js
@@ -1,13 +1,21 @@
 function serverRequest(url = '', method = '', data = null) {
-  let xhr = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
 
-  xhr.open(method, url);
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
-  if(data !== null) {
-    xhr.send(data);
-  } else {
-    xhr.send();
-  }
-  
-  return xhr.response;
+    xhr.onload = function() {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response)
+      }
+    }
+    xhr.onerror = function() {
+      reject(xhr.response);
+    }
+    xhr.send(JSON.stringify(data));
+  })
 }
